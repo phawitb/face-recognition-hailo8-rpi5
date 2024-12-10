844275fe77f9adab3a921f2454fbb2df31fbedb3
phawitb <96680592+phawitb@users.noreply.github.com>
2024-12-10 21:45:58 +0700

    Update FaceApp.sh

--- FaceApp.sh
+++ FaceApp.sh
@@ -12,13 +12,13 @@ sleep $10
 chromium --start-fullscreen http://localhost:8509 &
 
 # update faces 
-timeout 180 ./run/save_faces.sh --clean
+timeout 30 ./run/save_faces.sh --clean
 
 sleep $10
 while true; do
     # update faces
     echo "update faces data....."
-    ./run/save_faces.sh
+    timeout 10 ./run/save_faces.sh
 
     # run for 15 minutes(900sec) 
     timeout 900 python run/server.py --input usbcam   # edit source [usbcam, rpi, file] -----------------
