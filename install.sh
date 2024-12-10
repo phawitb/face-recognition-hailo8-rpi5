e1c16da41ffccf3968d11d626d3166fcd8c69012
phawitb <96680592+phawitb@users.noreply.github.com>
2024-12-10 12:58:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,19 +6,19 @@ source setup_env.sh
 # Install the required Python dependencies
 pip install -r requirements.txt 
 
-pip install -r tests/test_resources/requirements.txt
+# pip install -r tests/test_resources/requirements.txt
 
-# Install additional system dependencies (if needed)
-sudo apt install -y rapidjson-dev
+# # Install additional system dependencies (if needed)
+# sudo apt install -y rapidjson-dev
 
-# Check if the --all flag is provided
-DOWNLOAD_RESOURCES_FLAG=""
-if [[ "$1" == "--all" ]]; then
-    DOWNLOAD_RESOURCES_FLAG="--all"
-fi
+# # Check if the --all flag is provided
+# DOWNLOAD_RESOURCES_FLAG=""
+# if [[ "$1" == "--all" ]]; then
+#     DOWNLOAD_RESOURCES_FLAG="--all"
+# fi
 
-# Download resources needed for the pipelines
-./download_resources.sh $DOWNLOAD_RESOURCES_FLAG
+# # Download resources needed for the pipelines
+# ./download_resources.sh $DOWNLOAD_RESOURCES_FLAG
 
-# Optional: Post-process compilation (Only for older TAPPAS versions)
-./compile_postprocess.sh
+# # Optional: Post-process compilation (Only for older TAPPAS versions)
+# ./compile_postprocess.sh
